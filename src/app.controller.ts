224ccfcc06bfca339bb161e4c5dacec21dd4bd73
ronnie.hu <hulc_it@163.com>
2024-01-31 21:19:13 +0800

    feat: Provider的多种注入方式

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,35 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Inject } from '@nestjs/common';
 import { AppService } from './app.service';
+import { CatService } from './cat/cat.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  // constructor(private readonly appService: AppService) {}
+
+  // 属性注入和构造器注入是一样的效果
+  @Inject(AppService)
+  private readonly appService: AppService;
+
+  @Inject('cat_service')
+  private readonly catService: CatService;
+
+  @Inject('userinfo')
+  private readonly user: { name: string; age: number };
+
+  @Inject('info')
+  private readonly info: { name: string; age: number };
+
+  @Inject('mix')
+  private readonly mix: { name: string; info: string };
 
   @Get()
   getHello(): string {
-    return this.appService.getHello();
+    console.log(this.info);
+    return (
+      this.appService.getHello() +
+      this.catService.getName() +
+      ' ' +
+      JSON.stringify(this.user)
+    );
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,47 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PersonModule } from './person/person.module';
 import { OtherModule } from './other/other.module';
+import { CatService } from './cat/cat.service';
 
 @Module({
   imports: [PersonModule, OtherModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: 'cat_service',
+      useClass: CatService,
+    },
+    {
+      provide: 'userinfo',
+      useValue: {
+        name: 'ronnie',
+        age: 28,
+      },
+    },
+    {
+      provide: 'info',
+      useFactory() {
+        return {
+          name: 'hlongc',
+          age: 28,
+        };
+      },
+    },
+    {
+      provide: 'mix',
+      inject: ['userinfo', 'cat_service'],
+      useFactory(
+        userinfo: { name: string; age: number },
+        catService: CatService,
+      ) {
+        console.log(userinfo, catService);
+        return {
+          name: userinfo.name,
+          info: catService.getName(),
+        };
+      },
+    },
+  ],
 })
 export class AppModule {}
