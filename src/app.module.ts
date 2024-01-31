bbc5e754f78693c21c3b3b9c7a33ac2b9340d5ff
ronnie.hu <hulc_it@163.com>
2024-01-31 20:13:46 +0800

    feat: expors Service

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PersonModule } from './person/person.module';
+import { OtherModule } from './other/other.module';
 
 @Module({
-  imports: [PersonModule],
+  imports: [PersonModule, OtherModule],
   controllers: [AppController],
   providers: [AppService],
 })
