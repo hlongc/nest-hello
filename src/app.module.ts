5fdd39b2b94fc9401229e9104659c4af7998102d
ronnie.hu <hulc_it@163.com>
2024-02-01 21:06:23 +0800

    feat: 全局中间件、路由中间件

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
-import { Module } from '@nestjs/common';
+import { Module, MiddlewareConsumer, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { LogMiddleware } from './log.middleware';
 
 @Module({
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    // 当前中间件只对hello路由生效
+    consumer.apply(LogMiddleware).forRoutes('hello');
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { NextFunction, Request, Response } from 'express';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   // 静态资源预览
   app.useStaticAssets('public', { prefix: '/static' });
+  // 全局中间件
+  app.use((req: Request, res: Response, next: NextFunction) => {
+    console.log('before', req.url);
+    next();
+    console.log('after');
+  });
+
   await app.listen(3000);
 }
 bootstrap();
