cdf4e2487a20133ac49c0f8188c2e5f36dd33a38
hulongchao <hulc_it@163.com>
2024-02-05 17:15:28 +0800

    feat: 基础框架

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,5 @@
-import { Controller, Get, UseInterceptors } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-import { TimeoutInterceptor } from './timeout.interceptor';
 
 @Controller()
 export class AppController {
@@ -9,23 +8,5 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
-  }
-
-  @Get('world')
-  async getWorld(): Promise<string> {
-    await new Promise((resolve) => setTimeout(resolve, 2000));
-    return 'world';
-  }
-
-  @Get('hhh')
-  getHhh(): string {
-    return 'hhh';
-  }
-
-  @Get('timeout')
-  @UseInterceptors(TimeoutInterceptor)
-  async getTimeout(): Promise<string> {
-    await new Promise((resolve) => setTimeout(resolve, 4000));
-    return 'timeout';
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,24 +1,10 @@
 import { Module } from '@nestjs/common';
-import { APP_INTERCEPTOR } from '@nestjs/core';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TimerInterceptor } from './timer.interceptor';
-import { MapInterceptor } from './map.interceptor';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [
-    AppService,
-    {
-      // 全局使用拦截器
-      provide: APP_INTERCEPTOR,
-      useClass: TimerInterceptor,
-    },
-    {
-      provide: APP_INTERCEPTOR,
-      useClass: MapInterceptor,
-    },
-  ],
+  providers: [AppService],
 })
 export class AppModule {}
