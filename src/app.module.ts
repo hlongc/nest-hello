54dedd3cf2a1fb32df40d4bf366b691596a8cb50
hulongchao <hulc_it@163.com>
2024-02-05 15:20:16 +0800

    feat: 动态模块

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BbbModule } from './bbb/bbb.module';
 
 @Module({
-  imports: [],
+  imports: [BbbModule.register({ a: 1, b: 2 })],
   controllers: [AppController],
   providers: [AppService],
 })
