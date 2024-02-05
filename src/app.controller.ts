572a0467c109afae180b4402bd52b47f88632634
hulongchao <hulc_it@163.com>
2024-02-05 18:06:03 +0800

    feat: 系统内置pipe使用

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,23 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  ParseBoolPipe,
+  ParseFloatPipe,
+  ParseIntPipe,
+  Query,
+  HttpStatus,
+  ParseArrayPipe,
+  Param,
+  ParseEnumPipe,
+  DefaultValuePipe,
+} from '@nestjs/common';
 import { AppService } from './app.service';
 
+enum LevelEnum {
+  A = '1',
+  B = '2',
+  C = '3',
+}
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
@@ -8,5 +25,57 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('int')
+  getInt(@Query('num', ParseIntPipe) num: number): number {
+    return num;
+  }
+
+  @Get('float')
+  getFloat(@Query('num', ParseFloatPipe) num: number): number {
+    return num;
+  }
+
+  @Get('boolean')
+  getBoolean(
+    @Query(
+      'input',
+      new ParseBoolPipe({
+        errorHttpStatusCode: HttpStatus.GONE,
+      }),
+    )
+    input: boolean,
+  ): boolean {
+    return input;
+  }
+
+  @Get('array')
+  getArray(
+    @Query(
+      'list',
+      new ParseArrayPipe({
+        items: Number, // 指定每一项的类型
+        separator: ',', // 指定分隔符
+        optional: true, // 是否可选
+      }),
+    )
+    list: number[] = [],
+  ): number {
+    return list.reduce((memo, cur) => memo + cur, 0);
+  }
+
+  @Get('enum/:num')
+  getEnum(
+    @Param('num', new ParseEnumPipe(LevelEnum)) num: LevelEnum,
+  ): LevelEnum {
+    return num;
+  }
+
+  @Get('default')
+  getDefault(
+    @Query('name', new DefaultValuePipe(`I'm default value`)) name: string,
+  ): string {
+    return name;
   }
 }
